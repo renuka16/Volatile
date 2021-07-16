67ba144e83727aad0e6085fd4a97042d9ad10d95
Renuka <renuka.vadlamudi@microfocus.com>
2021-07-16 18:59:42 +0530

    v1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private final static int noOfThreads = 200000;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        VolatileData volatileData = new VolatileData();
+
+        Thread[] threads = new Thread[noOfThreads];     //creating Thread array
+        for(int i = 0; i < noOfThreads; ++i)
+            threads[i] = new VolatileThread(volatileData);
+
+        for(int i = 0; i < noOfThreads; ++i)
+            threads[i].start();
+
+        for(int i = 0; i < noOfThreads; ++i)
+            threads[i].join();
     }
 }
